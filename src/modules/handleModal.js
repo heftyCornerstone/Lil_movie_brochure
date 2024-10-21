b16fe5c2393d6a303b601f56bec875d053d904a2
sanha <heftycornerstone@gmail.com>
2024-10-21 12:35:44 +0900

    update : searh movies by title

--- src/modules/handleModal.js
+++ src/modules/handleModal.js
@@ -1,4 +1,4 @@
-import { getMovieByTitle } from "./getTmdbData.js"
+import { getMovieById } from "./getTmdbData.js"
 import { bookmarkParser } from "./showBookmarkList.js";
 
 const modalScreen = document.querySelector('#modalScreen');
@@ -13,18 +13,16 @@ const modalBookmarkBtn = document.querySelector('#modalBookmarkBtn');
 //모달창 컨텐츠 채우기
 async function generateModalContents(movieCard){
   const movieId = movieCard.getAttribute('id');
-  const movieTitle = movieCard.querySelector('.movieCard_info_inner_movieTitle').innerHTML;
-  const movieData = await getMovieByTitle(movieTitle);
-  const currentMovie = movieData.results[0];
+  const movieData = await getMovieById(movieId);
   const bookmarkSet = bookmarkParser();
-  const isBookmarked = (bookmarkSet) ? bookmarkSet.has(movieTitle) : null;
+  const isBookmarked = (bookmarkSet) ? bookmarkSet.has(movieId) : null;
 
   modal.setAttribute('id', movieId);
-  modalPoster.setAttribute('src',`https://image.tmdb.org/t/p/original/${currentMovie.poster_path}`);
-  modalTitle.innerHTML = currentMovie.title;
-  modalReleaseDate.innerHTML = `개봉일: ${currentMovie.release_date}`;
-  modalStars.innerHTML = `평점: ${Math.round(currentMovie.vote_average)}/10`;
-  modalOverview.innerHTML = (currentMovie.overview.length) ? currentMovie.overview : '시놉시스가 제공되지 않는 영화입니다';
+  modalPoster.setAttribute('src',`https://image.tmdb.org/t/p/original/${movieData.poster_path}`);
+  modalTitle.innerHTML = movieData.title;
+  modalReleaseDate.innerHTML = `개봉일: ${movieData.release_date}`;
+  modalStars.innerHTML = `평점: ${Math.round(movieData.vote_average)}/10`;
+  modalOverview.innerHTML = (movieData.overview.length) ? movieData.overview : '시놉시스가 제공되지 않는 영화입니다';
   modalBookmarkBtn.innerHTML = (isBookmarked) ? '북마크 삭제하기' : '북마크 추가하기';
 }
 
--- src/modules/searchMovies.js
+++ src/modules/searchMovies.js
@@ -1,5 +1,5 @@
 import { getMovieByTitle } from "./getTmdbData.js";
-import { appendMovieCardList } from "./paintMovieListView.js";
+import { appendMovieCardList, paintMovieListView } from "./paintMovieListView.js";
 
 const movieSearchbar = document.querySelector('#movieSearchbar');
 
@@ -7,13 +7,12 @@ async function searchMovies(e){
     const inputText = e.target.value.toLowerCase();
     const rawSearchedMovieData = await getMovieByTitle(inputText);
     const searchedMovieData = rawSearchedMovieData.results;
-    const movieTitles = [];
+    const movieIds = [];
     for(let i=0; i<searchedMovieData.length; i++){
-        const curMovieTitle = searchedMovieData[i].title;
-        movieTitles.push(curMovieTitle);
+        const curMovieId = searchedMovieData[i].id;
+        movieIds.push(curMovieId);
     }
-    console.log(searchedMovieData[0]);
-    console.log(movieTitles);
+    paintMovieListView(movieIds);
 }
 
 async function infiniteScroll(){
@@ -26,45 +25,3 @@ async function infiniteScroll(){
 }
 
 export {searchMovies}
-
-//검색하고픈 내용 submit하면 검색 결과를 띄우는 것이 요구사항
-//실시간 검색 결과 보여주기는 선택사항
-//검색시 대소문자 구분 없어야함
-
-/*
-페이지가 어디까지 있는지 알아야하겠지
-
-클로저로 스크롤 횟수를 기억, 그리고 그 횟수로 어떤 페이지 받아올건지 결정.
-
-스크롤이 바닥에 가까워지면 
-    계속해서 데이터를 받아옴, 맥스페이지까지.
-    인풋값이랑 기억하는 검색값이 같은지 다른지 매번 체크해서
-    만일 달라지면 스크롤 횟수를 초기화. 맥스페이지도.
-
-이 이벤트는 검색기능이 돌아가는 중에만 있을거고,
-북마크 페이지로 가거나
-홈으로 돌아가면 사라짐.
-removeEventListener()
-
-
-//pseudo code
-function infiniteScroll() {
-    let searchingKeyword = '';
-    let maxPage = ; 
-    let pageToLoad = 2; //초기값
-
-    return (e)=>{
-        const inputText = '';
-        if(inputText !== searchingKeyword) {
-            //searchingKeyword, maxPage, pageToLoad 재설정
-            searchingKeyword = inputText; 
-            maxPage = ;
-            pageToLoad = 2;
-        }
-        데이터 받아와서 채우기
-    }
-}
-
-
-
-*/
